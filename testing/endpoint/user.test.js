48089be8870ac957bb9f82aa681275e7ee1b38c0
Rizalfaizin06 <rizalfaizin06@gmail.com>
2024-03-21 15:43:20 +0700

    clear code user test

--- testing/endpoint/user.test.js
+++ testing/endpoint/user.test.js
@@ -7,8 +7,6 @@ describe("Test User Management", () => {
     afterEach(async () => {
         await User.destroy({ truncate: true, cascade: true });
 
-        // Clear users after each test case to ensure isolation
-        // await User.destroy({ truncate: true, cascade: true });
         if (server && server.close) {
             await new Promise((resolve) => server.close(resolve));
         }
@@ -60,4 +58,39 @@ describe("Test User Management", () => {
             }
         }
     );
+
+    // test("should retrieve 500 internal error", async () => {
+    //     jest.spyOn(User, "findAll").mockRejectedValue(
+    //         new Error("Database error")
+    //     );
+    //     const response = await supertest(app).get("/users");
+
+    //     expect(response.status).toBe(500);
+    //     expect(response.body).toEqual({ error: "Database error" });
+    // });
+
+    // test.each([
+    //     [0, 0],
+    //     [1, 1],
+    //     [50, 50],
+    // ])(
+    //     "should retrieve %i users (create %i users first)",
+    //     async (expectedUsers, numUsersToCreate) => {
+    //         jest.clearAllMocks();
+
+    //         for (let i = 0; i < numUsersToCreate; i++) {
+    //             await User.create({
+    //                 username: `user${i + 1}`,
+    //                 password: "test pw",
+    //                 createdAt: "2024-03-14T09:07:29.202Z",
+    //                 updatedAt: "2024-03-14T09:07:29.202Z",
+    //             });
+    //         }
+
+    //         const response = await supertest(app).get("/users");
+    //         expect(response.status).toBe(200);
+    //         expect(response.body).toHaveLength(expectedUsers);
+    //         await User.destroy({ truncate: true, cascade: true });
+    //     }
+    // );
 });
